fe6685253bd2fb7479b8eb8c6a553fd3dce06621
Patricio-Espinoza-Rojas <p.alfonsoespinoza@gmail.com>
2022-06-23 22:06:50 +0000

    update domain name generator

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function generate() {
+let pronoun = ['the', 'our', 'i', 'you', 'he', 'it', 'we', 'they'];
+let adj = ['great', 'big', 'hot', 'cold', 'warm', 'dry', 'ligth', 'clean']
+let noun = ['jogger', 'racoon', 'family', 'party', 'sky', 'baby', 'lady', 'library', 'puppy', 'fly']
 
-    let pronoun = ['The','our','',''];
-    let rand_pronoun = Math.floor(Math.random() * pronoun.length);
-    let adj = ['great','big','',''];
-    let rand_adj = Math.floor(Math.random() * adj.length);
-    let noun = ['jogger', 'racoon', ''];
-    let rand_noun = Math.floor(Math.random() * noun.length);
-    // let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
-    // let rand_when = Math.floor(Math.random() * when.length);
+let domain = ( pronoun, adj, noun ) => {
+    for (var p = 0; p < pronoun.length; p++) {
+        for (var a = 0; a < adj.length; a++) {
+            for (var n = 0; n < noun.length; n++) {
+                console.log(pronoun[i] + adj[j] + noun[l] + '.cl');
+            }
+        }
+    }
 
-    document.getElementById('result').innerHTML = "<h1>your domains is:</h1><div class='alert alert-success'><h2>" + pronoun[rand_pronoun] + " " + adj[rand_adj] + " " +noun[rand_noun] + "</h2></div>";
-}
+};
+domain(pronoun, adj, noun);
+
+
+// + '.com' + '.es' + '.org' + '.io' + '.us'
+
